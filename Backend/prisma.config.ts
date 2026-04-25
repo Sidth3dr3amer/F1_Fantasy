06320d8c264ccf357ac840549286a5e308e5f962
Sidth3dr3am3r <siddarth.potdar23@spit.ac.in>
2026-04-25 15:24:19 +0530

    database pushed and backend structure initialised fixed p7 error

--- Backend/prisma.config.ts
+++ Backend/prisma.config.ts
@@ -1,14 +1,3 @@
-import dotenv from 'dotenv'
-
-dotenv.config()
-export default {
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
-    },
-}
-
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
